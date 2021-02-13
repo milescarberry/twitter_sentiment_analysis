02c8ff2d3e327902db62088a8093222c0a880c22
milescarberry <62004569+milescarberry@users.noreply.github.com>
2021-02-13 20:25:46 +0530

    commit

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from textblob import TextBlob
 import tweepy
 
 import sys                # This module is part of the core Python stack.
+
+from dotenv import load_dotenv
+
+
+
+with open("keys.env") as env_file:
+
+    api_key, api_secret_key, bearer_token = load_dotenv(env_file, Verbose = True)
